cd4a239749e46957569b8c5620b6332a8e71860e
andrewapp <andrew.appuhamy@gmail.com>
2021-04-28 14:35:05 +1000

    hash table revision

--- mtask.py
+++ mtask.py
@@ -6,10 +6,10 @@ from multiprocessing import Process
 from em_service import sendemail
 import datetime
 import pickle
-
+import re
 #init(convert=True) #if no coloured font with output working use this line
 
-urls = ["https://www.pccasegear.com/products/52254/amd-ryzen-5-5600x-with-wraith-stealth",
+urls = ["https://www.pccasegear.com/products/53255/msi-geforce-rtx-3080-ventus-3x-10gb",
         "https://www.ple.com.au/Products/643561/AMD-Ryzen-5-5600X-37Ghz-6-Core-12-Thread-AM4---With-Wraith-Stealth-Cooler",
         "https://old.msy.com.au/amd-ryzen-5-5600x-100-100000065box-up-to-46ghz-base-clock-37ghzam46-cores12-threads32mb65w-unlocked-boxed-cpu-without-cpu-cooler",
         "https://www.umart.com.au/AMD-Ryzen-5-5600X-6-Core-AM4-4-6GHz-CPU-Processor_57284G.html",
@@ -19,13 +19,7 @@ i = 0
 
 checktxt = ["In stock", "In Stock", "in stock", "Call", "Yes"]
 stores = ["Online", "Bundoora"]
-inv = {
-    "PCCG": "Out of Stock",
-    "PLE": "Out of Stock",
-    "CCOM": "Out of Stock",
-    "MSY": "Out of Stock",
-    "Umart": "Out of Stock"
-}
+inv = {}
 
 body = []
 
@@ -33,7 +27,9 @@ body = []
 
 class MySpider(scrapy.Spider):
     name = "PCStockAU"
-    start_urls = ["https://www.pccasegear.com/products/52254/amd-ryzen-5-5600x-with-wraith-stealth"]
+    start_urls = [urls[0]]
+
+
     custom_settings = {
         'LOG_ENABLED': 'False',
 
@@ -46,68 +42,83 @@ class MySpider(scrapy.Spider):
         store = False
         j = 0
 
-        if i == 0:
+        if i == 0: #PCCG Instance
 
             pccg = response.xpath("//div[@class='price-box']//text()").getall()
+            title = response.xpath("//div[@class='title']//text()").getall()
+            key = f"{title[1]} @PCCG"
+            inv[key] = {}
             for stock in pccg:
                 j = j + 1
 
                 if checktxt[0] in stock:
-                    print(f"{Fore.BLUE}@PCCG::{Fore.GREEN}In Stock{Style.RESET_ALL}")
-                    inv["PCCG"] = "In Stock"
+                    inv[key] = "In Stock"
+                    print(f"{Fore.BLUE}{title[1]} @PCCG::{Fore.GREEN}In Stock{Style.RESET_ALL}")
                     store = True
+
                 elif j == len(pccg):  # 2nd last element in list will be ignored due to array indexing from 1 instead of 0
                     if store is False:
-                        print(f"{Fore.BLUE}@PCCG::{Fore.RED}Out of Stock{Style.RESET_ALL}")
+                        inv[key] = "Out of Stock"
+
+                        print(f"{Fore.BLUE}{title[1]} @PCCG::{Fore.RED}Out of Stock{Style.RESET_ALL}")
 
             i = i + 1
             yield scrapy.Request(urls[1], callback=self.parse)
 
 
-        elif i == 1:
+        elif i == 1: #PLE Instance
 
             ple = response.xpath("//div[@class='viewItemAvailabilityStatusWrapper']//text()").getall()
+            title = response.xpath("//div[@class='itemName']//text()").getall()
 
+            key = f"{title[0]} @PLE"
+            inv[key] = {}
             for stock in ple:
                 j = j + 1
 
                 if checktxt[0] in stock and store is False:
-
-                    print(f"{Fore.BLUE}@PLE::{Fore.GREEN}In Stock{Style.RESET_ALL}")
-
-                    inv["PLE"] = "In Stock"
+                    inv[key] = "In Stock"
+                    print(f"{Fore.BLUE}{title[0]} @PLE::{Fore.GREEN}In Stock{Style.RESET_ALL}")
                     store = True
 
 
 
                 elif j == len(ple):
                     if store is False:
+                        inv[key] = "Out of Stock"
                         print(f"{Fore.BLUE}@PLE::{Fore.RED}Out of Stock{Style.RESET_ALL}")
 
             i = i + 1
             yield scrapy.Request(urls[2], callback=self.parse)
 
 
-        elif i == 2:
+        elif i == 2: #MSY Instance
 
             msy = response.xpath("//div[@class='product-specs-box']//tr[@class='odd']//td[@class='spec-value ui-table-text-center color-green']//text()").get()
-            if msy:
-                print(f"{Fore.BLUE}@MSY::{Fore.GREEN}In Stock{Style.RESET_ALL}")
-                inv["MSY"] = "In Stock"
-            else:
-                print(f"{Fore.BLUE}@MSY::{Fore.RED}Out of Stock{Style.RESET_ALL}")
+            title = response.xpath("//h1[@itemprop='name']//text()").getall()
+            title = title[0]
 
+            title = title.replace("\r\n", "")
+            title = title.strip()
+
+            key = f"{title} @MSY"
+            inv[key] = {}
+            if msy:
+                inv[key] = "In Stock"
+                print(f"{Fore.BLUE}{title} @MSY::{Fore.GREEN}In Stock{Style.RESET_ALL}")
+            else:
+                inv[key] = "Out of Stock"
+                print(f"{Fore.BLUE}{title} @MSY::{Fore.RED}Out of Stock{Style.RESET_ALL}")
             i = i + 1
             yield scrapy.Request(urls[3], callback=self.parse)
 
-        elif i == 3:
+        elif i == 3: #Umart Instance
             umart = response.xpath("//div[@class='col-xs-12 col-sm-6 col-md-12']//div[@class='content']//text()").getall()
             for stock in umart:
                 j = j + 1
 
                 if checktxt[1] in stock:
                     print(f"{Fore.BLUE}@Umart::{Fore.GREEN}In Stock{Style.RESET_ALL}")
-                    inv["Umart"] = "In Stock"
                     break
 
                 elif j == len(umart):
@@ -116,7 +127,7 @@ class MySpider(scrapy.Spider):
             i = i + 1
             yield scrapy.Request(urls[4], callback=self.parse)
 
-        elif i == 4:
+        elif i == 4: #CCOM Instance
 
             ccom = response.xpath("//div[@class='prod_right']//text()").getall()
             for stock in ccom:
@@ -124,7 +135,6 @@ class MySpider(scrapy.Spider):
 
                 if checktxt[1] in stock:
                     print(f"{Fore.BLUE}@Centrecom::{Fore.GREEN}In Stock{Style.RESET_ALL}")
-                    inv["CCOM"] = "In Stock"
                     break
                 elif checktxt[3] in stock:
                     print(f"{Fore.BLUE}@Centrecom::{Fore.YELLOW}Call{Style.RESET_ALL}")
@@ -176,6 +186,7 @@ if __name__ == '__main__':
                 body = pickle.load(fi)
             if checktxt[1] in inv.values():
                 print("email sent")
+                print(inv)
                 sendemail("Product Status", body)
 
 
